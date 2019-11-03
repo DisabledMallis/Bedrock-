7cf94c1bd59682598a1489639f92ea4f48219b71
DisabledMallis <mallisiscool@gmail.com>
2019-11-03 10:15:32 -0500

    Only inject, let game handle key presses and everything!

--- MainClass.cs
+++ MainClass.cs
@@ -41,22 +41,9 @@ namespace Bedrock_
 
             Console.WriteLine("Injecting...");
             memlib.writeMemory("Minecraft.Windows.exe+9DEDDE", "bytes", "FF 25 00 00 00 00 45 D1 FB 30 F8 7F 00 00 90 90 90 90 90 90 90 90 90 90");
-            memlib.writeMemory("nvldumdx.dll+D145", "bytes", "F3 0F 10 0D 1E 00 00 00 F3 0F 10 B8 EC 00 00 00 F3 0F 10 80 E8 00 00 00 FF 25 00 00 00 00 F5 ED BD 54 F7 7F 00 00 00 00 20 41");
-            memlib.writeMemory("nvldumdx.dll+D16B", "float", "90");
+            memlib.writeMemory("nvldumdx.dll+D145", "bytes", "83 3D 30 DD DF CE 01 74 1D F3 0F 10 0D 35 00 00 00 F3 0F 10 B8 EC 00 00 00 F3 0F 10 80 E8 00 00 00 E9 8A 1C 8C CC F3 0F 10 0D 1C 00 00 00 F3 0F 10 B8 EC 00 00 00 F3 0F 10 80 E8 00 00 00 E9 6D 1C 8C CC CC CC CC 00 00 B4 42 00 00 F0 41 CC");
             Console.WriteLine("Injected!");
-
-            while (true)
-            {
-                Thread.Sleep(10);
-                if (memlib.readByte("Minecraft.Windows.exe+2F1AE7C") > 0)
-                {
-                    memlib.writeMemory("nvldumdx.dll+D16B", "float", "30");
-                }
-                else
-                {
-                    memlib.writeMemory("nvldumdx.dll+D16B", "float", "90");
-                }
-            }
+            Thread.Sleep(2000);
         }
     }
 }
