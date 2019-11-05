9fa4eb51543b71f58003f4adf894130fe6e90449
DisabledMallis <mallisiscool@gmail.com>
2019-11-04 23:08:00 -0500

    Permanently show coords

--- MainClass.cs
+++ MainClass.cs
@@ -44,6 +44,8 @@ namespace Bedrock_
             memlib.writeMemory("Minecraft.Windows.exe+9DEDDE", "bytes", "E9 00 3C 84 00 90 90 90 90 90 90 90 90 90 90 90 90 90 90 90 90 90 90 90");
             memlib.writeMemory("Minecraft.Windows.exe+12229E3", "bytes", "83 3D 92 84 CF 01 01 74 1D F3 0F 10 0D 35 00 00 00 F3 0F 10 B8 EC 00 00 00 F3 0F 10 80 E8 00 00 00 E9 EC C3 7B FF F3 0F 10 0D 1C 00 00 00 F3 0F 10 B8 EC 00 00 00 F3 0F 10 80 E8 00 00 00 E9 CF C3 7B FF CC CC CC 00 00 B4 42 00 00 F0 41 CC");
 
+            memlib.writeMemory("Minecraft.Windows.exe+5D72CD", "bytes", "90 90 90 90 90 90");
+            
             Console.WriteLine("Injected!");
             Thread.Sleep(2000);
         }
